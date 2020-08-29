e773f203f06e748a83133390546afa8b8c2e2f11
suzhi82 <suzhi82@gmail.com>
2020-08-29 17:35:34 +1000

    add options

--- src/genlog.py
+++ src/genlog.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import getopt
 import json
+import os
 import sys
 
 
-def main():
+def main(argv):
     confs_file = 'spec.json'
     output_file = 'test.log'
     line_count = 100
     schar = 'á'
+    endofline = '\n'
+    usage = argv[0] + ' -c <confsfile> -o <outputfile> -l <linecount>'
 
     try:
-        json_fp = open(confs_file, mode='r')
-        confs = json.load(json_fp)
+        opts, args = getopt.getopt(argv[1:], "hc:o:l:")
+    except getopt.GetoptError as err:
+        print(err)
+        print(usage)
+        sys.exit(1)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage)
+            sys.exit(0)
+        elif opt == '-c':
+            confs_file = arg
+        elif opt == '-o':
+            output_file = arg
+        elif opt == '-l':
+            line_count = int(arg)
+
+    try:
+        confs_fp = open(confs_file, mode='r')
+        confs = json.load(confs_fp)
 
         fields = list(map(lambda x: '%' + x + 's', confs['Offsets']))
         print(fields)
@@ -21,8 +42,11 @@ def main():
         line_str = ''
         for f in fields:
             line_str += f % schar
-        line_str += '\n'
+        line_str += endofline
 
+        output_dir = os.path.dirname(output_file)
+        if output_dir:
+            os.mkdir(output_dir)
         output_fp = open(output_file, mode='w', encoding=confs['FixedWidthEncoding'])
 
         print(output_fp.encoding)
@@ -37,8 +61,8 @@ def main():
         print("Unexpected error:", sys.exc_info()[0])
 
     finally:
-        if 'json_fp' in dir():
-            json_fp.close()
+        if 'confs_fp' in dir():
+            confs_fp.close()
 
         if 'output_fp' in dir():
             output_fp.flush()
@@ -46,4 +70,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
