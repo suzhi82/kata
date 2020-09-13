28dd9f023278f0a360d40a68eb0e02d31ccbe5fe
suzhi82 <suzhi82@gmail.com>
2020-09-13 22:07:53 +1000

    v0.99b

--- src/test_parser.py
+++ src/test_parser.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from genfixed import gen_fixed
 from parser import intercept, parser
 
 
@@ -18,8 +19,25 @@ class TestParser(unittest.TestCase):
         pos = [1, 2, 3, 4]
         self.assertEqual(intercept(line, pos), ['A', 'BB', 'CCC', 'DDDD'])
 
-    # compare the content of fixed width file and delimited file
+    # generate fixed width file test
     def test_case03(self):
+        lines1 = [
+            "â" * 5 + "ú" * 12 + "õ" * 3 + "â" * 2 + "ú" * 13 + "õ" * 7 + "â" * 10 + "ú" * 13 + "õ" * 20 + "Ñ" * 13 + "\n",
+            "â" * 5 + "ú" * 12 + "õ" * 3 + "â" * 2 + "ú" * 13 + "õ" * 7 + "â" * 10 + "ú" * 13 + "õ" * 20 + "Ñ" * 13 + "\n",
+            "â" * 5 + "ú" * 12 + "õ" * 3 + "â" * 2 + "ú" * 13 + "õ" * 7 + "â" * 10 + "ú" * 13 + "õ" * 20 + "Ñ" * 13
+        ]
+        file_name = 'fixed.txt'
+        gen_fixed(file_name)
+        with open(file_name, mode='r', encoding='cp1252') as f:
+            lines2 = []
+            line = f.readline()
+            while line:
+                lines2.append(line)
+                line = f.readline()
+        self.assertEqual(lines1, lines2)
+
+    # compare the content of fixed width file and delimited file
+    def test_case04(self):
         include_header = True
 
         with open('fixed.txt', mode='r', encoding='cp1252') as file1, \
@@ -29,7 +47,7 @@ class TestParser(unittest.TestCase):
             line1 = file1.readline()
             line2 = file2.readline()
             while line1 or line2:
-                self.assertEqual(line1.strip('\n'), line2.replace(',', '').strip('\n'))
+                self.assertEqual(line1.strip('\n'), line2.strip('\n').replace(',', ''))
                 line1 = file1.readline()
                 line2 = file2.readline()
 
